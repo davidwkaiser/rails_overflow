d03d5c43a5059f63df4df763f19978337ef1c425
Dave Kaiser <email": null>
2016-05-02 11:39:13 -0500

    additional question functionality

--- rails_overflow/app/controllers/questions_controller.rb
+++ rails_overflow/app/controllers/questions_controller.rb
@@ -3,12 +3,16 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-
   def show
     @question = Question.find(params[:id])
   end
 
   def new
+    @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
@@ -19,6 +23,23 @@ class QuestionsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    redirect_to questions_path
   end
 
   private
