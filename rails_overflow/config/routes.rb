d1daaf0e1e9c340de7d3dfb55e94e600d8e8485b
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-02 23:24:59 -0500

    add virtual attributes

--- rails_overflow/config/routes.rb
+++ rails_overflow/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-
-
-
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -14,15 +11,11 @@ Rails.application.routes.draw do
   post  'login' =>  'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-
-
   resources :users
 
   resources :questions do
     resources :answers
   end
-
-
 
 
   root 'welcome#index'
